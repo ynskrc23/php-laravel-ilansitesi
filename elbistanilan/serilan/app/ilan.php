dc1748bd342c33d29dcf7b5e4b6b5c7018286600
Yunus Karaca <ynskrc82@gmail.com>
2022-02-04 22:30:15 +0300

    first commit

--- elbistanilan/serilan/app/ilan.php
+++ elbistanilan/serilan/app/ilan.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class ilan extends Model
 {
-    //
+    //test
     protected $table ='tbl_ilan';
     protected $primaryKey = 'ilan_id';
     protected $fillable = ['kat_id','users_id','ilan_baslik','ilan_adi','ilan_soyadi','ilan_email','ilan_adresi','ilan_fiyat','ilan_telefon','ilan_detay','ilan_image'];
-  
+
     public function picture()
    {
       return $this->hasMany('App\picture','picture_ilan_id','ilan_id');
